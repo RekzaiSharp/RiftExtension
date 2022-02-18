23ed1eadf348b04732deb4225d1f70b62709b26b
BGXGG <95216913+BGXGG@users.noreply.github.com>
2022-02-18 15:59:51 +0600

    Update dllmain.cpp

--- DivineSeries/dllmain.cpp
+++ DivineSeries/dllmain.cpp
@@ -2,19 +2,19 @@
 #include "../plugin_sdk/plugin_sdk.hpp"
 
 PLUGIN_NAME("DivineSeries");
+SUPPORTED_CHAMPIONS(champion_id::Irelia);
 
 PLUGIN_API bool on_sdk_load(plugin_sdk_core* plugin_sdk_good)
 {
     DECLARE_GLOBALS(plugin_sdk_good);
 
-    console->print("DivineSeries Loaded");
-
 	switch (myhero->get_champion())
 	{
 	case champion_id::Irelia:
 		Irelia::OnLoad(); 
-	default:
 		break;
+	default:
+		return false;
 	}
 
     return true;
@@ -26,6 +26,7 @@ PLUGIN_API void on_sdk_unload()
 	{
 	case champion_id::Irelia:
 		Irelia::OnUnload();
+		break;
 	default:
 		break;
 	}
